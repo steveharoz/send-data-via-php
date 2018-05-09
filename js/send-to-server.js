b7df778d3247732cd7a2aae62ab305bf17bbd1d7
Steve Haroz <steveharoz@users.noreply.github.com>
2018-05-09 11:46:09 +0200

    Initial commit

---
+++ js/send-to-server.js
@@ -0,0 +1,51 @@
+function showFinished() {
+	$('#sending').fadeOut().promise().done(() => $('#finished').fadeIn());
+	$('#subjectID').text(experiment.subjectID);
+}
+function showError(message) {
+	$('#sending').fadeOut().promise().done(() => $('#error').fadeIn());
+	$('#errorMessage').text(message);
+}
+
+function cleanDataBeforeSending (experimentResults) {
+	// compute duration (you need to set duration to performance.now() when the experiment starts)
+	experimentResults.duration = performance.now() - experimentResults.duration;
+
+    // shallow copy
+    cleanedExperiment = jQuery.extend({}, experimentResults);
+    // drop unneeded properties
+    delete cleanedExperiment.metadataThatWeDontNeed;
+	
+	return cleanedExperiment;
+}
+
+function sendJSON(experimentResults, experimentName = "myexperiment", subjectID, experimentDate, quickCheckInfo=100, callback) {
+	// show size of block data
+	console.log("block size (bytes): " + encodeURIComponent(JSON.stringify(experimentResults, null, " ")).length);
+
+	// put the parameters in the url
+	var parameters = '?' +
+		'study=' + encodeURIComponent(experimentName) + '&' +
+		'subjectID=' + encodeURIComponent(subjectID) + '&' +
+		'date=' + encodeURIComponent(experimentDate) + '&' +
+		'accuracy=' + encodeURIComponent(quickCheckInfo);
+
+	console.log('server/save-json.php' + parameters);
+
+	// send
+	fetch('server/save-json.php' + parameters, {
+		method: 'POST',
+		headers: {"content-type": "application/json"},
+		body: JSON.stringify(experimentResults, null, " ")
+	})
+	.then(response => response.text())
+	.then(response => {
+		console.log(response)
+		if (response.endsWith("Yay Success!"))
+			showFinished();
+		else
+			showError(response);
+	});
+
+	return;
+}
---
+++ server/save-json.php
@@ -0,0 +1,32 @@
+<?php
+
+$study = rtrim($_GET['study']);
+$subjectID = rtrim($_GET['subjectID']);
+$date = rtrim($_GET['date']);
+$accuracy = rtrim($_GET['accuracy']);
+$data = file_get_contents('php://input');
+
+// sterilize  directory
+if (strpos($study, '..') !== false) {
+    die("Bad location");
+}
+
+// set and optionally make directory
+$dir = "../data/";
+if (!file_exists($dir)) {
+    echo "making directory: " . $dir. "\n";
+	mkdir($dir);
+}
+$dir = $dir . $study;
+if (!file_exists($dir)) {
+    echo "making directory: " . $dir. "\n";
+	mkdir($dir);
+}
+
+// save the data
+$fh = fopen($dir . "/" . $subjectID . " acc_" . $accuracy . " date_" . $date . ".json", 'w') or die("Error opening file!");
+fwrite($fh, $data);
+fclose($fh);
+
+echo "Yay Success!";
+?>
